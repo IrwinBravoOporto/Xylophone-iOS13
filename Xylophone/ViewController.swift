1182ae357e6eb2a495bdcc6466dcb28bd2a4f07c
irwin <irwinbravooporto@gmail.com>
2020-11-19 10:37:25 -0500

    proyecto finish

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +20,24 @@ class ViewController: UIViewController {
 
 
     
+    @IBAction func keyPressed(_ sender: UIButton) {
+       
+        playSound(valueSong: sender.currentTitle!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+              //Bring's sender's opacity back up to fully opaque.
+              sender.alpha = 1.0
+          }
+    }
+    
 
+    func playSound(valueSong:String) {
+        guard let url = Bundle.main.url(forResource: "\(valueSong)", withExtension: "wav") else { return }
+        
+        player = try! AVAudioPlayer(contentsOf: url)
+        player.play()
+
+    }
+    
 }
 
